9a0736072d6ac469681c9aae5a76b4ecf5f9232e
lcc <948200163@qq.com>
2022-07-22 17:29:59 +0800

    init

--- alias.config.js
+++ alias.config.js
@@ -2,15 +2,14 @@
  * webstorm的webpack映射文件
  * 应该雨vite。config中alias的配置保持一致
  * */
-import path from 'path';
-const resolve = (dir) => path.join(__dirname, dir);
+const resolve = (dir) => require('path').join(__dirname, dir);
 
 module.exports = {
   resolve: {
     alias: {
       '@': resolve('src'),
-      '@c': path.resolve(__dirname, 'src/components'),
-      '@p': path.resolve(__dirname, 'src/pages'),
+      '@c': resolve('src/components'),
+      '@p': resolve('src/pages'),
     },
   },
 };
