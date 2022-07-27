8486c6c016d4237ebc72cac16b19fe183d90b2d5
lcc <948200163@qq.com>
2022-07-27 18:18:51 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ const routes = [
     path: '/',
     name: 'Index',
     component: () => import('@/pages/index/index'),
+    meta: { title: '首页' },
   },
 ];
 
@@ -16,5 +17,16 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
+
+router.afterEach((to) => {
+  document.title = to.meta.title;
+});
+
+// 获取原型对象上的push函数
+const originalPush = VueRouter.prototype.push;
+// 修改原型对象中的push方法
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => err);
+};
 
 export default router;
